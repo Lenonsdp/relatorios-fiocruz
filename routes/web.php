871207e5f1bee9177c874899d74e9789d96a2c4b
Lenon <lenon.paula@bling.com.br>
2023-12-05 00:25:13 -0300

    Feat: Normaliza dados para renderizar

--- routes/web.php
+++ routes/web.php
@@ -30,12 +30,56 @@ Route::get('/relatorio', function (\Illuminate\Http\Request $request) {
 		'dateStart' => $request->input('dateStart'),
 		'dateEnd' => $request->input('dateEnd'),
 		'operator' => $request->input('operator'),
-		'lote' => $request->input('lote')
+		'lote' => $request->input('lote'),
+		'ciclo' => $request->input('ciclo')
 	];
 
-    $results = DB::connection('sqlsrv')->select(DB::raw("SELECT * FROM StringTable WHERE Val LIKE '" . $request->input('lot') . "'"));
+	$connection = DB::connection('sqlsrv');
+	$connection2 = DB::connection('sqlsrv2');
+	$condicao = false;
+	if (!empty($data['lote'])) {
+		$condicao .=  "Val LIKE '" . $request->input('lote') . "' AND TagIndex = 4 ";
+	} else if(!empty($data['operator'])) {
+		$condicao .= !$condicao ? " Val = '" . $request->input('operator') . "' AND TagIndex = 3" : "OR (Val LIKE '" . $request->input('lote') . "' AND TagIndex = 3)";
+	} else if (!empty($data['ciclo'])) {
+		$condicao .= !$condicao ? " Val = '" . $request->input('ciclo') . "' AND TagIndex = 5" : "OR (Val LIKE '" . $request->input('ciclo') . "' AND TagIndex = 5)";
+	}
 
+	$results = $connection->select(DB::raw("SELECT * FROM StringTable WHERE " . $condicao));
+	$millitm_values = array_unique(array_map(function($results) {
+		return (int) $results->Millitm;
+	}, $results));
 
+	$resultsFill = $connection->select(DB::raw("SELECT * FROM StringTable WHERE Millitm in (".implode(',', $millitm_values).")"));
+	$resultsNormalized = [];
 
-	return $results;
+	foreach ($resultsFill as $result) {
+		$resultsNormalized[$result->Millitm][$result->TagIndex] = $result;
+	}
+
+	$resultsFloat = $connection->select(DB::raw("SELECT * FROM FloatTable WHERE Millitm in (".implode(',', $millitm_values).")"));
+
+	foreach ($resultsFloat as $resultFloat) {
+		$resultsNormalized[$resultFloat->Millitm][$resultFloat->TagIndex] = $resultFloat;
+	}
+
+	//* Mydb2 relatórios//
+
+	$resultsDbRelatorios = $connection2->select(DB::raw("SELECT * FROM StringTable WHERE Millitm in (".implode(',', $millitm_values).")"));
+
+	foreach ($resultsDbRelatorios as $resultDbRelatorios) {
+		$resultsNormalized[$resultDbRelatorios->Millitm]['relatorios'][$resultDbRelatorios->TagIndex] = $resultDbRelatorios;
+	}
+	$resultsDbRelatoriosFloat = $connection2->select(DB::raw("SELECT * FROM FloatTable WHERE Millitm in (".implode(',', $millitm_values).")"));
+
+	foreach ($resultsDbRelatoriosFloat as $resultDbRelatoriosFloat) {
+		$resultsNormalized[$resultDbRelatoriosFloat->Millitm]['relatorios'][$resultDbRelatoriosFloat->TagIndex] = $resultDbRelatoriosFloat;
+	}
+
+	$resultDatas = $connection->select(DB::raw("SELECT MIN(DateAndTime) as MinDate, MAX(DateAndTime) as MaxDate FROM StringTable WHERE Millitm in (".implode(',', $millitm_values).")"));
+
+	return [
+		'results' => $resultsNormalized,
+		'datas' => $resultDatas
+	];
 });
