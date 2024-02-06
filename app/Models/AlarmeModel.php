6947518963399a9df7e570cea76af59a3c24039b
Lenon <lenon.paula@bling.com.br>
2024-02-05 21:18:38 -0300

    Feat: Adiciona regra no model de alarme -3 horas;

--- app/Models/AlarmeModel.php
+++ app/Models/AlarmeModel.php
@@ -8,6 +8,11 @@ class AlarmeModel {
 
 	public function getDataAlarmeTable($dataMin, $dataMax) {
 		$connection = DB::connection('alarme');
+
+		// Subtract 3 hours from $dataMin and $dataMax
+		$dataMin = now()->parse($dataMin)->subHours(3);
+		$dataMax = now()->parse($dataMax)->subHours(3);
+
 		return $connection->table('AllEvent')
 			->select(
 				'ServerName',
